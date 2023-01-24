ae648d96dd85c6227465364b8983bc7780a6dc74
sangeui <sangeuis@icloud.com>
2023-01-24 17:02:45 +0900

    화면 개발 편리를 위해 루트 뷰 컨트롤러를 그리드 레이아웃 뷰 컨트롤러로 교체했습니다.

--- Sample/Sample/App Delegates/SceneDelegate.swift
+++ Sample/Sample/App Delegates/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = .init(windowScene: windowScene)
-        self.window?.rootViewController = self.createHomeViewController()
+        self.window?.rootViewController = GridLayoutViewController()
         self.window?.makeKeyAndVisible()
     }
 }
