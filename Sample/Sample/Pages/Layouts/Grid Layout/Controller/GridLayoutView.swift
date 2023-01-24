d0a5873a15273254148ac0873bf6acb5685f39cf
sangeui <sangeuis@icloud.com>
2023-01-24 16:46:35 +0900

    그리드 레이아웃 페이지를 위한 컨트롤러와 뷰 껍데기를 만들었습니다.

---
+++ Sample/Sample/Pages/Layouts/Grid Layout/Controller/GridLayoutView.swift
@@ -0,0 +1,12 @@
+//
+//  GridLayoutView.swift
+//  Sample
+//
+//  Created by William on 2023/01/24.
+//
+
+import UIKit
+
+class GridLayoutView: UIView {
+    
+}
---
+++ Sample/Sample/Pages/Layouts/Grid Layout/Controller/GridLayoutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GridLayoutViewController.swift
+//  Sample
+//
+//  Created by William on 2023/01/24.
+//
+
+import UIKit
+
+class GridLayoutViewController: UIViewController {
+    override func loadView() {
+        self.view = GridLayoutView()
+    }
+}
